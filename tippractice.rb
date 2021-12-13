442796e37900e86290f3f6043e32723774f96426
pjonasson <88997326+pjonasson@users.noreply.github.com>
2021-12-13 12:07:02 -0600

    Conversion of hash with hashes that have multiple values into single key and value hash

--- tippractice.rb
+++ tippractice.rb
@@ -461,12 +461,107 @@ string = "leetcode"
 # Input: {"a" => 1, "b" => 2, "c" => 3}
 # Output: {1 => "a", 2 => "b", 3 => "c"}
 
-def flip_hash(hash)
+# def flip_hash(hash)
+#   answer = {}
+#   hash.each do |k, v|
+#     answer[v] = k
+#   end
+#   return answer
+# end
+
+# p flip_hash({ "a" => 1, "b" => 2, "c" => 3 })
+
+# You are given a hash in format #A, and you are to return the same data as a hash using format #B, as specified below:
+
+# Input:
+
+{ 1 => ["A", "E", "I", "O", "U"] }
+
+# Output:
+
+# {
+# 'a' => 1,
+# 'e' => 1,
+# 'i' => 1,
+# 'o' => 1,
+# 'u' => 1
+# }
+# Input:
+
+# {
+# 1 => ["A", "E"],
+# 2 => ["D", "G"]
+# }
+
+# Output:
+
+# {
+# 'a' => 1,
+# 'd' => 2,
+# 'e' => 1,
+# 'g' => 2
+# }
+
+# Input:
+
+# {
+# 1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+# 2 => ["D", "G"],
+# 3 => ["B", "C", "M", "P"],
+# 4 => ["F", "H", "V", "W", "Y"],
+# 5 => ["K"],
+# 8 => ["J", "X"],
+# 10 => ["Q", "Z"]
+# }
+
+# Output:
+
+# {
+# 'a' => 1,
+# 'b' => 3,
+# 'c' => 3,
+# 'd' => 2,
+# 'e' => 1,
+# 'f' => 4,
+# 'g' => 2,
+# 'h' => 4,
+# 'i' => 1,
+# 'j' => 8,
+# 'k' => 5,
+# 'l' => 1,
+# 'm' => 3,
+# 'n' => 1,
+# 'o' => 1,
+# 'p' => 3,
+# 'q' => 10,
+# 'r' => 1,
+# 's' => 1,
+# 't' => 1,
+# 'u' => 1,
+# 'v' => 4,
+# 'w' => 4,
+# 'x' => 8,
+# 'y' => 4,
+# 'z' => 10
+# }
+
+def hash_output(hash)
   answer = {}
   hash.each do |k, v|
-    answer[v] = k
+    array = v
+    array.each do |n|
+      answer[n] = k
+    end
   end
-  return answer
+  return answer.sort_by { |key| key }.to_h
 end
 
-p flip_hash({ "a" => 1, "b" => 2, "c" => 3 })
+p hash_output({
+    1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+    2 => ["D", "G"],
+    3 => ["B", "C", "M", "P"],
+    4 => ["F", "H", "V", "W", "Y"],
+    5 => ["K"],
+    8 => ["J", "X"],
+    10 => ["Q", "Z"],
+  })
