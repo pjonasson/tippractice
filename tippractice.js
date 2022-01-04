2737b8514c2448e16cb96cd9369bc7b87f6eeea9
pjonasson <88997326+pjonasson@users.noreply.github.com>
2022-01-04 10:49:34 -0600

    Javascript sum of array

--- tippractice.js
+++ tippractice.js
@@ -53,3 +53,22 @@
 // }
 
 // console.log(dnaTranscription("ACGTGGTCTTAA"));
+
+// Write a function that returns the sum of all numbers in a given array.
+
+// NOTE: Do not use any built-in language functions that do this automatically for you.
+
+// Input: [1, 2, 3, 4]
+// Output: 10
+
+// Explanation: (1 + 2 + 3 + 4) = 10
+
+function sum(array) {
+  var sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
+}
+
+console.log(sum([1, 8, 8, 4]));
