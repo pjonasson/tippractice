137cff783e5462ea9657f3a5228a13767f8a2c52
pjonasson <88997326+pjonasson@users.noreply.github.com>
2022-01-05 10:57:49 -0600

    Javascript checking two equal strings to see difference in characters. Returns count.

--- tippractice.js
+++ tippractice.js
@@ -256,16 +256,38 @@
 // Input: “baloney”
 // Output: false
 
-function palindrome(string) {
-  var reverse = "";
-  for (let i = string.length - 1; i >= 0; i--) {
-    reverse += string[i];
+// function palindrome(string) {
+//   var reverse = "";
+//   for (let i = string.length - 1; i >= 0; i--) {
+//     reverse += string[i];
+//   }
+//   if (reverse === string) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(palindrome("racecar"));
+
+// Given two strings of equal length, write a function that returns the number of characters that are different between the two strings.
+
+// Input: "ABCDEFG", "ABCXEOG"
+// Output: 2
+
+// Explanation: While the A, B, C, E, and G are in the same place for both strings, they have different characters in the other spaces. Since there are 2 such spaces that are different (the D and F in the first string), we return 2.
+
+// Input: "ABCDEFG", "ABCDEFG",
+// Output: 0
+
+function hamming(string1, string2) {
+  let count = 0;
+  for (let i = 0; i < string1.length; i++) {
+    if (string1[i] !== string2[i]) {
+      count += 1;
+    }
   }
-  if (reverse === string) {
-    return true;
-  } else {
-    return false;
-  }
+  return count;
 }
 
-console.log(palindrome("racecar"));
+console.log(hamming("ABCDEFG", "ABXDERG"));
