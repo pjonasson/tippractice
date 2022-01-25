9caf4795613d448af6df7d62d33be7777c03d61d
pjonasson <88997326+pjonasson@users.noreply.github.com>
2022-01-25 12:22:44 -0600

    Ruby Popular Posts

--- tippractice.rb
+++ tippractice.rb
@@ -756,3 +756,34 @@ string = "leetcode"
 # end
 
 # p subset([1, 2, 3, 4, 5, 6], [1, 6, 3, 20])
+
+# Given an array of hashes that represent a list of social media posts, return a new array that only contains the posts that have at least 1000 likes.
+
+# Input: [
+# {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+# {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+# {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+# {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+# ]
+
+# Output: [
+# {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+# {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+# ]
+
+def popular_posts(x)
+  output = []
+  x.each do |post|
+    if post[:likes] >= 1000
+      output << post
+    end
+  end
+  return output
+end
+
+p popular_posts([
+    { title: "Me Eating Pizza", submitted_by: "Joelle P.", likes: 1549 },
+    { title: "i never knew how cool i was until now", submitted_by: "Lyndon Johnson", likes: 3 },
+    { title: "best selfie evar!!!", submitted_by: "Patti Q.", likes: 1092 },
+    { title: "Mondays are the worst", submitted_by: "Aunty Em", likes: 644 },
+  ])
